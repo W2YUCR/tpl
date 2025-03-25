a45b3990ca280329c0d521f20053b5ae2bd79cf0
W2YUCR <146299962+W2YUCR@users.noreply.github.com>
2025-03-25 01:18:18 -0700

    Implement basic operators

--- tpl/tpl/interpreter.cpp
+++ tpl/tpl/interpreter.cpp
@@ -41,29 +41,50 @@ class Interpreter : public ast::ExprVisitor {
 	}
 
   private:
-	std::map<std::string, Value> scope{
-		{"+",
-		 [&](ast::Expr &lhs, ast::Expr &rhs) -> Value {
-			 return std::visit(
-				 overloaded{
-					 [](std::int64_t lhs, std::int64_t rhs) -> Value { return lhs + rhs; },
-					 [](Value lhs, Value rhs) -> Value {
-						 throw std::runtime_error{std::format("Cannot add {} and {}", lhs, rhs)};
-					 },
-				 },
-				 eval(lhs), eval(rhs));
-		 }},
+	template <class Op>
+	class AutoOp {
+	  public:
+		AutoOp(Interpreter *interpreter, char const *name) : _interpreter{interpreter}, _name{name} {}
+		auto operator()(ast::Expr &lhs, ast::Expr &rhs) -> Value
 		{
-			"=",
-			[&](ast::Expr &lhs, ast::Expr &rhs) -> Value {
-				if (auto *var = dynamic_cast<ast::Variable *>(&lhs)) {
-					return scope[var->name] = eval(rhs);
-				}
-				else {
-					throw std::runtime_error{std::format("The LHS is not a variable")};
-				}
-			},
-		}};
+			return std::visit(overloaded{
+				[](auto lhs, auto rhs) -> Value
+					requires requires { Op{}(lhs, rhs); }
+				{ return Op{}(lhs, rhs); },
+				[this](Value lhs, Value rhs) -> Value {
+					throw std::runtime_error{std::format("Cannot call {} with {} and {}", _name, lhs, rhs)};
+				},
+				},
+				_interpreter->eval(lhs),
+				_interpreter->eval(rhs));
+		};
+
+	  private:
+		Interpreter *_interpreter;
+		char const *_name;
+	};
+
+	template <class Op>
+	auto auto_op(char const *name)
+	{
+		return AutoOp<Op>{this, name};
+	}
+
+	std::map<std::string, Value> scope{
+		{"+", AutoOp<std::plus<>>(this, "plus")},
+		{"-", AutoOp<std::minus<>>{this, "minus"}},
+		{"/", AutoOp<std::divides<>>{this, "divide"}},
+		{"*", AutoOp<std::multiplies<>>{this, "multiply"}},
+		{"=",
+		 [&](ast::Expr &lhs, ast::Expr &rhs) -> Value {
+			 if (auto *var = dynamic_cast<ast::Variable *>(&lhs)) {
+				 return scope[var->name] = eval(rhs);
+			 }
+			 else {
+				 throw std::runtime_error{std::format("The LHS is not a variable")};
+			 }
+		 }},
+	};
 
 	Value eval(ast::Expr &expr)
 	{
